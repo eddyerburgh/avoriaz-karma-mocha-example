9809cce76133198e3ca38e2e3caa858bbe21308e
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-01-17 18:15:35 +0000

    add missing semicolons in build and config directories

--- build/build.js
+++ build/build.js
@@ -1,26 +1,26 @@
 // https://github.com/shelljs/shelljs
-require('./check-versions')()
-require('shelljs/global')
-env.NODE_ENV = 'production'
+require('./check-versions')();
+require('shelljs/global');
+env.NODE_ENV = 'production';
 
-var path = require('path')
-var config = require('../config')
-var ora = require('ora')
-var webpack = require('webpack')
-var webpackConfig = require('./webpack.prod.conf')
+var path = require('path');
+var config = require('../config');
+var ora = require('ora');
+var webpack = require('webpack');
+var webpackConfig = require('./webpack.prod.conf');
 
 console.log(
   '  Tip:\n' +
   '  Built files are meant to be served over an HTTP server.\n' +
   '  Opening index.html over file:// won\'t work.\n'
-)
+);
 
-var spinner = ora('building for production...')
-spinner.start()
+var spinner = ora('building for production...');
+spinner.start();
 
 webpack(webpackConfig, function (err, stats) {
-  spinner.stop()
-  if (err) throw err
+  spinner.stop();
+  if (err) throw err;
   process.stdout.write(stats.toString({
     colors: true,
     modules: false,
@@ -28,4 +28,4 @@ webpack(webpackConfig, function (err, stats) {
     chunks: false,
     chunkModules: false
   }) + '\n')
-})
+});
--- build/check-versions.js
+++ build/check-versions.js
@@ -1,10 +1,10 @@
-var semver = require('semver')
-var chalk = require('chalk')
-var packageConfig = require('../package.json')
+var semver = require('semver');
+var chalk = require('chalk');
+var packageConfig = require('../package.json');
 var exec = function (cmd) {
   return require('child_process')
     .execSync(cmd).toString().trim()
-}
+};
 
 var versionRequirements = [
   {
@@ -17,12 +17,12 @@ var versionRequirements = [
     currentVersion: exec('npm --version'),
     versionRequirement: packageConfig.engines.npm
   }
-]
+];
 
 module.exports = function () {
-  var warnings = []
+  var warnings = [];
   for (var i = 0; i < versionRequirements.length; i++) {
-    var mod = versionRequirements[i]
+    var mod = versionRequirements[i];
     if (!semver.satisfies(mod.currentVersion, mod.versionRequirement)) {
       warnings.push(mod.name + ': ' +
         chalk.red(mod.currentVersion) + ' should be ' +
@@ -32,14 +32,14 @@ module.exports = function () {
   }
 
   if (warnings.length) {
-    console.log('')
+    console.log('');
     console.log(chalk.yellow('To use this template, you must update following to modules:'))
-    console.log()
+    console.log();
     for (var i = 0; i < warnings.length; i++) {
-      var warning = warnings[i]
+      var warning = warnings[i];
       console.log('  ' + warning)
     }
-    console.log()
+    console.log();
     process.exit(1)
   }
 }
