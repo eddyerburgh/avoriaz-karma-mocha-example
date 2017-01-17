02e94ebde51effc369ef38863cf9501a416d96fd
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-01-17 18:01:34 +0000

    add click method and test

--- test/unit/specs/Hello.spec.js
+++ test/unit/specs/Hello.spec.js
@@ -3,8 +3,24 @@ import Hello from 'src/components/Hello';
 import {mount} from 'avoriaz';
 
 describe('Hello.vue', () => {
-  it('renders an h1 containing correct text', () => {
+  it('renders an h1', () => {
     const wrapper = mount(Hello);
-    expect(wrapper.find('h1')[0].text()).to.equal('Welcome to Your Vue.js App');
+    expect(wrapper.find('h1').length).to.equal(1);
+  });
+
+  it('h1 renders data.msg as text', () => {
+    const wrapper = mount(Hello);
+    const msg = wrapper.data().msg;
+    expect(wrapper.find('h1')[0].text()).to.equal(msg);
+  });
+
+  it('h1 text changes when button is clicked', () => {
+    const expectedMessage = 'new message';
+
+    const wrapper = mount(Hello);
+    const button = wrapper.find('#change-message')[0];
+    button.simulate('click');
+
+    expect(wrapper.find('h1')[0].text()).to.equal(expectedMessage);
   });
 });
