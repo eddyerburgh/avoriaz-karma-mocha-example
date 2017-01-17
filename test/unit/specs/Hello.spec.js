4208efbc2fc99e86b5fa08fd9644562bb009d183
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-01-17 17:49:25 +0000

    use avoriaz to test Hello.vue

--- test/unit/specs/Hello.spec.js
+++ test/unit/specs/Hello.spec.js
@@ -1,13 +1,10 @@
 import Vue from 'vue';
 import Hello from 'src/components/Hello';
+import {mount} from 'avoriaz';
 
 describe('Hello.vue', () => {
-  it('should render correct contents', () => {
-    const vm = new Vue({
-      el: document.createElement('div'),
-      render: (h) => h(Hello),
-    });
-    expect(vm.$el.querySelector('.hello h1').textContent)
-      .to.equal('Welcome to Your Vue.js App');
+  it('renders an h1 containing correct text', () => {
+    const wrapper = mount(Hello);
+    expect(wrapper.find('h1')[0].text()).to.equal('Welcome to Your Vue.js App');
   });
 });
