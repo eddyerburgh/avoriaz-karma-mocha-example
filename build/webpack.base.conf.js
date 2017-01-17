5aefae65df567d82cfc51bdcc10094e4960b166b
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-01-17 18:17:25 +0000

    remove unused variables from webpack.base.conf

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,14 +1,7 @@
-var path = require('path')
-var config = require('../config')
-var utils = require('./utils')
-var projectRoot = path.resolve(__dirname, '../')
-
-var env = process.env.NODE_ENV
-// check env & config/index.js to decide whether to enable CSS source maps for the
-// various preprocessor loaders added to vue-loader at the end of this file
-var cssSourceMapDev = (env === 'development' && config.dev.cssSourceMap)
-var cssSourceMapProd = (env === 'production' && config.build.productionSourceMap)
-var useCssSourceMap = cssSourceMapDev || cssSourceMapProd
+var path = require('path');
+var config = require('../config');
+var utils = require('./utils');
+var projectRoot = path.resolve(__dirname, '../');
 
 module.exports = {
   entry: {
